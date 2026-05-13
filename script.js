716c35bd404bf05023cea4127417433535db0ecb
KelvinSilvaVigor <kelvin.silva@vigor.com.br>
2026-05-13 11:00:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log("Glossário online 🚀");
+const searchInput = document.getElementById("searchInput");
+
+searchInput.addEventListener("keyup", () => {
+
+  const value = searchInput.value.toLowerCase();
+
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+
+    const text = card.innerText.toLowerCase();
+
+    card.style.display =
+      text.includes(value)
+      ? "block"
+      : "none";
+
+  });
+
+});
+
+/* COPY BUTTON */
+
+const copyButtons = document.querySelectorAll(".copy-btn");
+
+copyButtons.forEach(button => {
+
+  button.addEventListener("click", () => {
+
+    const code =
+      button
+        .closest(".card")
+        .querySelector("code")
+        .innerText;
+
+    navigator.clipboard.writeText(code);
+
+    button.innerText = "✅ Copiado";
+
+    setTimeout(() => {
+      button.innerText = "📋 Copiar";
+    }, 1500);
+
+  });
+
+});
