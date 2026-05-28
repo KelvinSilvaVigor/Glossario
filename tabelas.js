cee17ab075499719642b7e9ec04ea919eeadc19a
KelvinSilvaVigor <kelvin.silva@vigor.com.br>
2026-05-28 15:11:57 -0300

    tabelas.js

--- tabelas.js
+++ tabelas.js
@@ -1,53 +1,9 @@
-const tabelas = [
+console.log("arquivo js carregou");
 
-  {
-    nome: "DimCalendario",
-    tipo: "Dimensão",
-    descricao: "Tabela calendário criada em Linguagem M"
-  },
+document.addEventListener("DOMContentLoaded", () => {
 
-  {
-    nome: "DimClientes",
-    tipo: "Dimensão",
-    descricao: "Base PFT 2026ap"
-  },
-
-  {
-    nome: "DimTransportador",
-    tipo: "Dimensão",
-    descricao: "Base PFT 2026ap"
-  },
-
-  {
-    nome: "DimVendedor",
-    tipo: "Dimensão",
-    descricao: "Base PFT 2026ap"
-  },
-
-  {
-    nome: "Regional_Logistica",
-    tipo: "Dimensão",
-    descricao: "Tabela logística regional"
-  },
-
-  {
-    nome: "PEX 1031",
-    tipo: "Fato",
-    descricao: "Retorno por SKU"
-  },
-
-  {
-    nome: "PFT 1302",
-    tipo: "Fato",
-    descricao: "Faturamento"
-  }
-
-];
-
-const container =
-  document.getElementById("tablesContainer");
-
-tabelas.forEach(table => {
+  const container =
+    document.getElementById("tablesContainer");
 
   const card =
     document.createElement("section");
@@ -58,16 +14,16 @@ tabelas.forEach(table => {
 
     <div class="doc-header">
 
-      <h2>${table.nome}</h2>
+      <h2>TESTE</h2>
 
       <span class="doc-badge">
-        ${table.tipo}
+        FUNCIONOU
       </span>
 
     </div>
 
     <p class="doc-description">
-      ${table.descricao}
+      JavaScript renderizou corretamente.
     </p>
 
   `;
